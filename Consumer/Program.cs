d6e18a2d653a6d0fcb1774bc5120aca3c330e381
Automerge <automerge@rts-tender.ru>
2023-03-09 22:26:25 +0300

    Add Masstransit

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -13,12 +13,13 @@ namespace Consumer
             var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
                 Configure(cfg);
-                RegisterEndPoints(cfg, int.Parse(args[0]));
+                RegisterEndPoints(cfg, 0/*int.Parse(args[0])*/);
             });
 
             await busControl.StartAsync();
             try
             {
+                throw new Exception();
                 Console.WriteLine("Press enter to exit");
                 await Task.Run(() => Console.ReadLine());
             }
@@ -34,12 +35,12 @@ namespace Consumer
         /// <param name="configurator"></param>
         private static void Configure(IRabbitMqBusFactoryConfigurator configurator)
         {
-            configurator.Host("hawk.rmq.cloudamqp.com",
-                "ykziztbb",
+            configurator.Host("moose.rmq.cloudamqp.com",
+                "wxuscjul",
                 h =>
                 {
-                    h.Username("ykziztbb");
-                    h.Password("oZaUpy2Sru1P0b04K9ghjx3MSFpXTMIU");
+                    h.Username("wxuscjul");
+                    h.Password("d86v39oHoWxr-ZWWZpDTO6uVhdBlUTVv");
                 });
         }
 
@@ -50,16 +51,16 @@ namespace Consumer
         /// <param name="index"></param>
         private static void RegisterEndPoints(IRabbitMqBusFactoryConfigurator configurator, int index)
         {
-            configurator.ReceiveEndpoint($"masstransit_event_queue_{index}", e =>
-            {
-                e.Consumer<EventConsumer>();
-                e.UseMessageRetry(r =>
-                {
-                    //r.Ignore<ArithmeticException>();
-                    r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-                });
-            });
-            configurator.ReceiveEndpoint($"masstransit_request_queue", e =>
+            //configurator.ReceiveEndpoint($"masstransit_event_queue_{index}", e =>
+            //{
+            //    e.Consumer<EventConsumer>();
+            //    e.UseMessageRetry(r =>
+            //    {
+            //        //r.Ignore<ArithmeticException>();
+            //        r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            //    });
+            //});
+            configurator.ReceiveEndpoint($"masstransit", e =>
             {
                 e.Consumer<RequestConsumer>();
             });
